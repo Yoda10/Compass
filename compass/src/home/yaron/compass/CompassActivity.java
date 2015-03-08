30082c9b5f464725141526d1a3468819848aadb6
Yaron Ronen <YaronAndAnat@gmail.com>
2015-03-08 12:50:46 +0200

    Register mobile device on GCM service and register the regId on the mobile server.

--- compass/src/home/yaron/compass/CompassActivity.java
+++ compass/src/home/yaron/compass/CompassActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -23,13 +24,21 @@ import com.google.android.gms.gcm.GoogleCloudMessaging;
  */
 public class CompassActivity extends Activity implements OnClickListener
 {	
+	// Constants
 	final static String TAG = CompassActivity.class.getSimpleName();
+	final static String API_PROJECT_NUMBER = "231729049897";
+	final static String API_SERVER_KEY = "AIzaSyAd7A0j7NOqoAyQRu3R-xBaVIDODoe-_aM";
+	
+	// Members
+	private TextView mainText = null;	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_compass);	
+		setContentView(R.layout.activity_compass);
+		
+		mainText = (TextView)findViewById(R.id.main_text);
 	}	
 
 	@Override
@@ -39,25 +48,31 @@ public class CompassActivity extends Activity implements OnClickListener
 
 		switch (v.getId()) {
 		case R.id.dummy_button:
-			registerDeviceOnServer();
+			//registerDeviceOnServer();
+			gcmRegistration();
 			break;
 		default:
 			break;
 		}		
 	}
 
-	private void registerDeviceOnServer()
+	/**
+	 * Sends the registration ID to your server over HTTP, so it can use GCM/HTTP
+	 * or CCS to send messages to your app.
+	 */
+	private void registerDeviceOnServer(final String deviceName, final String regId)
 	{
-		//final String URL = "http://192.168.0.105:8089/api/scores/register";
-		final String URL = "http://192.168.0.105:8089/api/scores/sendMessage";
+		final String URL = "http://192.168.0.104:8089/api/scores/register";
+		//final String URL = "http://192.168.0.104:8089/api/scores/sendMessage";
 		//final String URL = "http://192.168.0.105:8089/api/scores/test";
 
 		// Formulate the request and handle the response.
 		StringRequest stringRequest = new StringRequest(Request.Method.POST, URL,
 				new Response.Listener<String>() {
 			@Override
-			public void onResponse(String response) {
+			public void onResponse(String response) {				
 				Log.d(TAG,"onResponse:\n"+response.toString());
+				mainText.append("\nServer response:"+response.toString());
 			}
 		},
 		new Response.ErrorListener() {
@@ -65,15 +80,16 @@ public class CompassActivity extends Activity implements OnClickListener
 			public void onErrorResponse(VolleyError error) {
 				// Handle error
 				Log.d(TAG,"onErrorResponse:\n"+error.getMessage());
+				mainText.append("\nServer error response:"+error.getMessage());
 			}
 		})
 		{
 			@Override
 			protected Map<String,String> getParams() {
-				Map<String,String> params = new HashMap<String, String>();
-				params.put("RegistrationId","RegistrationId_55");
-				params.put("DeviceName","DeviceName_55");
-				params.put("Message","This is my Message.");
+				Map<String,String> params = new HashMap<String, String>();				
+				params.put("DeviceName",deviceName);
+				params.put("RegistrationId",regId);
+				//params.put("Message","This is my Message.");
 
 				return params;
 			}
@@ -95,35 +111,30 @@ public class CompassActivity extends Activity implements OnClickListener
 	public void gcmRegistration()
 	{
 		new AsyncTask<Void, Void, String>() {
+			String message = "empty";
 			@Override
 			protected String doInBackground(Void... params) {
-				String regId = null;
+				String regId = null;				 
 				try
 				{
 					final GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(getApplicationContext());
-					regId = gcm.register("PROJECT_NUMBER");					
-					Log.d(TAG,"Device successfully registered to GCM service regId:"+regId);
-					sendRegistrationIdToBackend(regId);
+					regId = gcm.register(API_PROJECT_NUMBER);
+					message = "Device successfully registered to GCM service."+"\nregId:"+regId;
+					Log.d(TAG,message);
+					registerDeviceOnServer("Sony_tablet",regId);
 				}
 				catch(Exception ex) {
-					Log.e(TAG, "Problem registering to GCM service.", ex);
+					message = "Problem registering to GCM service.";
+					Log.e(TAG, message, ex);					
 				}
 				return regId;
 			}
 
 			@Override
 			protected void onPostExecute(String regId) {
-
+				if( mainText != null )
+					mainText.setText(message);
 			}
 		}.execute(null, null, null);
-	}
-
-	/**
-	 * Sends the registration ID to your server over HTTP, so it can use GCM/HTTP
-	 * or CCS to send messages to your app.
-	 */
-	private void sendRegistrationIdToBackend(String regId)
-	{
-		// Your implementation here.
-	}
+	}	
 }
